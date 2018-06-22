6a7cafd9fb6c12b557c4ff22aa8d8064e28c9167
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-22 16:19:13 +0800

    add log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 export default (options = { snakecase: true }) => store => {
   store.subscribe((mutation, state) => {
+    console.log('plugin called')
+
     const isPromise = mutation.payload && mutation.payload.then
 
     if (!isPromise) {
       return
     }
+
+    console.log('keep working')
 
     const succededString = options.snakecase ? '_SUCCEEDED' : 'Succeeded'
     const failedString = options.snakecase ? '_FAILED' : 'Failed'
