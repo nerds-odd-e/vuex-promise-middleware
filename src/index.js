c7d264156d4dd084e927d91d4714bde14138576e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-22 17:04:00 +0800

    refactoring: remove all unneeded code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 export default store => {
-  store.subscribe((mutation, state) => {
-    console.log('plugin called')
-
-    const isPromise = mutation.payload && mutation.payload.then
-
-    if (!isPromise) {
-      return
-    }
-
-    console.log('keep working')
-
-    mutation.payload.then((response) => {
-      store.commit(`${mutation.type}_SUCCEEDED`, response)
-    }).catch((response) => {
-      store.commit(`${mutation.type}_FAILED`, response)
+    store.subscribe((mutation, state) => {
+        if (mutation.payload && mutation.payload.then) {
+            mutation.payload.then((response) => {
+                store.commit(`${mutation.type}_SUCCEEDED`, response)
+            }).catch((response) => {
+                store.commit(`${mutation.type}_FAILED`, response)
+            })
+        }
     })
-  })
 }
